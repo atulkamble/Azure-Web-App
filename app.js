71f939cb72d03f8036404b6e63acaaee7b57c2ee
Atul Kamble <atul_kamble@hotmail.com>
2025-02-10 14:35:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,12 @@
 const express = require('express');
-const sql = require('mssql');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Connect to Azure SQL Database
-const config = {
-  user: 'mywebapp-server-admin',
-  password: 'N7lZDxc$wht9O$jk',
-  server: 'mywebapp-server.database.windows.net',
-  database: 'mywebapp-database',
-  options: {
-    encrypt: true, // Use encryption
-    trustServerCertificate: false, // Use SSL certificate
-  },
-};
-
-sql.connect(config)
-  .then(() => console.log('Connected to Azure SQL Database'))
-  .catch(err => console.error('SQL Connection Error:', err));
-
 app.get('/', (req, res) => {
-  res.send('Hello, Azure!');
+  res.send('Hello, World!');
 });
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
